c3441add9c75f54c447978a973098a00b8963a7d
ptrkdy <patrick.r.daly@gmail.com>
2024-01-27 14:51:19 -0800

    gitignore fastapi boilerplate

--- app/main.py
+++ app/main.py
@@ -1,3 +1,58 @@
 #!/usr/bin/env python3
 
+from fastapi import FastAPI
+
+# ----------------------------------------------------------------------------
+# FastAPI as main entrypoint of the app for consumption
+# ----------------------------------------------------------------------------
+
+app = FastAPI()
+
+# ----------------------------------------------------------------------------
+# design goal: supply an intermediate proxy to RAG endpoint via fastAPI
+# ----------------------------------------------------------------------------
+
+# static response
+
+static_response = {
+    "text": "this response is static from the fastAPI nlg endpoint!",
+    "buttons": [],
+    "image": None,
+    "elements": [],
+    "attachments": []
+}
+
+# ----------------------------------------------------------------------------
+# some initial endpoints
+# ----------------------------------------------------------------------------
+
+
+# root
+@app.get("/")
+async def root():
+    return static_response
+
+# listener for event emitter
+@app.get("/incident/")
+async def root():
+    return static_response
+
+# endpoint to trigger RAG pipeline
+@app.get("/retrieve/")
+async def root():
+    return static_response
+
+
+# ----------------------------------------------------------------------------
+# enable post endpoint
+# ----------------------------------------------------------------------------
+
+# @app.post("/dynamic/")
+# async def request_gpt(nlg: incomplete):
+#     meta_response = static_response
+#     return meta_response
+
+
+# ----------------------------------------------------------------------------
 # EOF
+# ----------------------------------------------------------------------------
